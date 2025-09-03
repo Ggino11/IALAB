2a5c36cc2fd9c2f5bc8bc375f040cb6f92c2c9c9
Simone Amitrano <amitrano.simone@educ.di.unito.it>
2025-09-03 10:29:40 +0200

    Ida_star + utils per puzzle_8

---
+++ Puzzle_8/ida_star.pl
@@ -0,0 +1,56 @@
+%:- consult(utils,regole,heuristic,dominioP8).
+:- dynamic initial_state/1, wrapper_count_missplaced/2, print_state/1, goal/1, move/2, print_final_solution/1.
+:- set_prolog_flag(answer_write_options, [quoted(true), portray(true), max_depth(0)]). % to avoid abbreviation in the outpu lists
+
+% iterative deeping A* search alg f(n) = g(n) + h(n)
+ida_star(SolutionPath,Cost):-
+    initial_state(InitialState), % get initial state and count h(n)
+    wrapper_count_missplaced(InitialState,H), %H heuristic value result of count 
+    ida_loop([[InitialState]],H,SolutionPath,Cost).
+
+ida_loop(Paths, Threshold, SolutionPath, Cost):-
+    bounded_search(Paths,Threshold,Result, NewThreshold),
+    ( Result = found(SolutionPath,Cost) 
+    -> true
+    ;% format("No solution under ~w. Next threshold = ~w\n\n", [Threshold, NextThreshold]),
+    ida_loop(Paths, NewThreshold, SolutionPath, Cost)
+    ).
+
+% ricerca a profondità limitata su f
+bounded_search([], _, no_solution, inf). % no more paths to explore
+bounded_search([Path|Other], Threshold, Result, MinAbove) :-
+    Path = [Current|_],
+    length(Path, L), 
+    G is L - 1,                          % Cost from start to current node
+    wrapper_count_missplaced(Current, H), % Heuristic estimate
+    F is G + H,                          % Total cost estimate
+    
+    % % Debug printof each sate
+    % format("Current state (g=~w, h=~w, f=~w):\n", [G, H, F]),
+    % print_grid(Current), nl,             % Print current grid state
+    
+    ( goal(Current) ->                   % If current state is the goal
+        reverse(Path, SolutionPath),     % Reverse path to get correct order
+        Result = found(SolutionPath, G), % Return solution
+        MinAbove = Threshold,
+        
+        % Print the full solution path
+        writeln('\nPath to reaach final state:'),
+        print_final_solution(SolutionPath)
+        
+    ; F > Threshold ->                   % If cost exceeds threshold
+        bounded_search(Other, Threshold, Result2, Min2),
+        MinAbove is min(F, Min2),       % Track minimum exceeding cost
+        Result = Result2
+        
+    ;                                   % Else expand current node
+        findall(
+            [Next|Path],
+            ( move(Current, Next),       % Generate valid moves
+              \+ member(Next, Path)      % Avoid cycles
+            ),
+            NextPaths
+        ),
+        append(NextPaths, Other, AllPaths),
+        bounded_search(AllPaths, Threshold, Result, MinAbove)
+    ).
--- Puzzle_8/utils.pl
+++ Puzzle_8/utils.pl
@@ -19,3 +19,41 @@ swap_elements(List, Index1, Index2, NewList):-
     set_elem(List, Index1, Elem2, TempList),
     %replace the element at Index2 with the element at Index1
     set_elem(TempList, Index2, Elem1, NewList).
+
+
+% print_grid(State)
+% Prints the 3x3 grid for a single state
+print_grid([A,B,C,D,E,F,G,H,I]) :-
+    format('+---+---+---+\n'),
+    format('| ~w | ~w | ~w |\n', [A,B,C]),
+    format('+---+---+---+\n'),
+    format('| ~w | ~w | ~w |\n', [D,E,F]),
+    format('+---+---+---+\n'),
+    format('| ~w | ~w | ~w |\n', [G,H,I]),
+    format('+---+---+---+\n').
+
+% print_state(State)
+% Prints the move made between two states in a descriptive way
+print_move(PrevState, CurrentState) :-
+    nth0(EmptyIndex, CurrentState, e),
+    nth0(EmptyIndex, PrevState, MovedTile),  % The tile that moved into the empty space
+    MovedTile \= e,
+    nth0(TileIndex, PrevState, e),
+    format("Move tile ~w from position ~w to position ~w (empty)\n", [MovedTile, TileIndex, EmptyIndex]).
+
+% print_solution(SolutionPath)
+% Prints the entire solution path as a sequence of moves
+print_solution([]).
+print_solution([_]). % Single state (initial state)
+print_solution([State1, State2|Rest]) :-
+    print_move(State1, State2),
+    print_solution([State2|Rest]).
+
+print_final_solution(SolutionPath) :-
+    print_solution(SolutionPath),
+    last(SolutionPath, FinalState),
+    writeln('\nFinal state:'),
+    print_grid(FinalState),
+    length(SolutionPath, Length),
+    Cost is Length - 1,
+    format('\nTotal moves: ~w\n', [Cost]).
